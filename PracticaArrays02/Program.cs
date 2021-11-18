467f0274ebc5f3f87739862c1ed83299a306aafc
Josué Hernández <josuehernandez2314@gmail.com>
2021-11-17 22:23:23 -0600

    Add project files.

---
+++ PracticaArrays02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PracticaArrays02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombreEstudiantes;
+
+            Console.WriteLine("Cuantos estudiantes hay?");
+            int estudiantes = int.Parse(Console.ReadLine());
+
+            nombreEstudiantes = new string[estudiantes];
+
+
+            for (int i = 0; i < estudiantes; i++)
+            {
+                Console.WriteLine("Ingrese el nombre del estudiante");
+                nombreEstudiantes[i] = Console.ReadLine();
+            }
+
+            for(int j = 0; j < estudiantes; j++)
+            {
+                Console.WriteLine(nombreEstudiantes[j]);
+            }
+        }
+    }
+}
